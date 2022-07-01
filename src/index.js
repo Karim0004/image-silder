c4b30faed2b11956aa88e7ed86e99daf55f1a9f4
Karim <karimx099@gmail.com>
2022-07-01 17:23:21 +0200

    add slider buttons functionality

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
 /* eslint-disable no-param-reassign */
 export default function ImageSlider(...imageSources) {
+  if (imageSources.length < 1) return null;
+
   const slider = document.createElement('div');
+
   slider.style.minHeight = '150px';
   slider.style.minWidth = '200px';
   slider.style.maxWidth = '1000px';
   slider.style.maxHeight = '750px';
-  slider.style.backgroundColor = '#eeeeee';
+  slider.style.backgroundColor = '#555555';
   slider.style.aspectRatio = '1.5 / 1';
   slider.style.width = '100%';
+  slider.style.position = 'relative';
+  slider.style.padding = '0 24px';
+  slider.style.boxSizing = 'border-box';
 
   const frame = document.createElement('div');
   const container = document.createElement('div');
   container.style.display = 'flex';
   container.style.width = 'max-content';
   container.style.height = '100%';
+
   frame.appendChild(container);
   slider.appendChild(frame);
 
   frame.style.overflow = 'hidden';
-  frame.style.width = '90%';
+  frame.style.width = '100%';
   frame.style.margin = '0 auto';
-  frame.style.height = '90%';
+  frame.style.height = '95%';
 
   const items = [];
   imageSources.forEach((source) => {
@@ -29,7 +36,9 @@ export default function ImageSlider(...imageSources) {
       item.style.display = 'flex';
       item.style.justifyContent = 'center';
       item.style.alignItems = 'center';
+
       item.style.backgroundColor = '#cccccc';
+
       item.style.height = '100%';
       item.style.aspectRatio = '1 / 1';
 
@@ -44,6 +53,55 @@ export default function ImageSlider(...imageSources) {
       container.appendChild(item);
     }
   });
+
+  // sliding functionality
+  let currentSlide = 0;
+  container.style.transition = 'transform 0.4s ease-in-out';
+
+  function showSlide(slide) {
+    container.style.transform = `translateX(${
+      -(slide * items[0].offsetWidth)
+      - (frame.offsetWidth / 2)
+      + items[0].offsetWidth
+    }px)`;
+    currentSlide = slide;
+  }
+
+  function nextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  function previousSlide() {
+    showSlide(currentSlide - 1);
+  }
+
+  // sliding buttons
+  const previous = document.createElement('div');
+  previous.innerHTML = `<svg xmlns=
+  "http://www.w3.org/2000/svg" height="48" 
+  width="48"><path d="M28.05 36 16 23.95 28.05 
+  11.9l2.15 2.15-9.9 9.9 9.9 9.9Z"/></svg>`;
+  previous.style.position = 'absolute';
+  previous.style.top = '50%';
+  previous.style.left = '0';
+
+  const next = document.createElement('div');
+  next.innerHTML = `<svg xmlns=
+  "http://www.w3.org/2000/svg" height="48" 
+  width="48"><path d="m18.75 36-2.15-2.15 
+  9.9-9.9-9.9-9.9 2.15-2.15L30.8 23.95Z"/></svg>`;
+  next.style.position = 'absolute';
+  next.style.top = '50%';
+  next.style.right = '0';
+
+  const sliderCircles = document.createElement('div');
+  sliderCircles.style.backgroundColor = '#101010';
+  sliderCircles.style.height = '5%';
+
+  next.onclick = nextSlide;
+  previous.onclick = previousSlide;
+
+  slider.append(previous, next, sliderCircles);
 
   return slider;
 }
